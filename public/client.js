4bd20b60aace16b9bf2285cecc512aeda2c0137f
yusardv <yusardv@pop-os.localdomain>
2020-12-20 19:00:51 +0300

    H

--- public/client.js
+++ public/client.js
@@ -3,22 +3,30 @@ var firebaseConfig = {
   authDomain: "argem-dergisi.firebaseapp.com",
   databaseURL: "https://argem-dergisi-default-rtdb.firebaseio.com",
   projectId: "argem-dergisi",
-  storageBucket: "argem-dergisi.appspot.com",
+  storageBucket: "argem-dergisi.appspot.com",//Yarda Html Aç
   messagingSenderId: "588198437409",
   appId: "1:588198437409:web:4b8f4e0fe6e3b86cb3b960",
   measurementId: "G-SHMTQ59F9X"
 };
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
+
 //Base Variables
-const Database=firebase.database;
-//Base Functions
-function AddTextToDataBase(WritingName,WritersName,Writing,Topic,WritingCodename){
-  let DatabasePort=Database().ref("/"+WritingCodename);
-  DatabasePort.set({
-    Yazar:WritersName,
-    YazıAdı:WritingName,
-    Yazı:Writing,
-    YazıKoonusu:Topic,
-  });
+const database=firebase.database();
+
+var ref = database.ref('Yazılar')
+ref.on("value",gotData,gotError);
+function gotData(data){
+  var write = data.val();
+  var keys = Object.keys(write)
+  for (var i = 0;i < 3; i++){
+    var k = keys[i];
+function addData(data)
+  //Yusuf ne yaptın benim koduma
+  //
+  }
 }
+function gotError(err){
+  console.log("Error!")
+}
+//Base Functions
